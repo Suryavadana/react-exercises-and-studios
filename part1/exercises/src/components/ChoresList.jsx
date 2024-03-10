1e24b65c828d22257be04ae008542090b52bc897
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-03-10 18:32:27 -0500

    React Part1/Exercises

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './ChoresList.module.css';
-export default function ChoresList () {
-   return (<div>
-      <h3>Chores</h3>
-      <ol>
-      <li>Empty Dishwasher</li>
-      <li>Complete Prep Work</li>
-      <li>Buy Groceries</li>
-      </ol>
-      </div>);
+import styles from './ChoresList.module.css'; // Note the change here
+export default function ChoresList() {
+   return (
+      <div>
+         <h3 className={styles.choresHeading}>Chores</h3> {/* Apply CSS class */}
+         <ol>
+            <li className={styles.choresText}>Empty Dishwasher</li>
+            <li className={styles.choresText}>Complete Prep Work</li>
+            <li className={styles.choresText}>Buy Groceries</li>
+         </ol>
+      </div>
+   );
 }
