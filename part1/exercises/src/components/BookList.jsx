547ccad805a1aa38dbd3bf1f0641ff7de4a5d4a3
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-03-10 17:40:21 -0500

    Part1 /part1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,16 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Best Books";
+   let book1 = "https://m.media-amazon.com/images/I/81KVRenYv+L._SY522_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/71sERFosMeL._SY522_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/71ZdvpJTYbL._SY522_.jpg";
 
    return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+      <div >
+         <h3 style={{color:"blue", textDecoration:"underline" , fontStyle:"italic"}}>{pageTitle} </h3>
+         <img src={book1} alt="SUDHA MURTHY- Here there and everywhere!!" width= "100" height="200"/>
+         <img src={book2} alt="A.P.J.ABDUL KALAM-Learning How to Fly!!"  width= "190" height="230"/>
+         <img src={book3} alt="Dr. RANGAN CHATTERJEE-Happy Mind Happy Life!!"  width= "100" height="200" />
+       
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,10 @@
+import React from 'react';
+import './ChoresList.module.css';
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (<div>
+      <h3>Chores</h3>
+      <li>Empty Dishwasher</li>
+      <li>Complete Prep Work</li>
+      <li>Buy Groceries</li>
+      </div>);
 }
