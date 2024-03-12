b9dce64d1cce7845807b1ed1d4e88bd4308b103c
Surya Vadana Tetali Venkata <suryavadana5@outlook.com>
2024-03-12 18:08:37 -0500

    React part2 exercises

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,13 +1,28 @@
 import './styles.css';
+import oceans from './oceans.json';
 import Button from './Button';
 
-
+const listItems = oceans.map(oceans =>
+   <div key={oceans.id}  className={`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`}>
+   <img src={oceans.image} alt={oceans.name} className="img"/>
+   <h1>{oceans.name}</h1>
+   <h3>Fun Facts:</h3>
+   <ul>
+      <li>{oceans.fact1}</li>
+      <li>{oceans.fact2}</li>
+      <li>{oceans.fact3}</li>
+   </ul>
+   <Button />
+   </div>
+   );
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
+         <ul>
+            {listItems}
+         </ul>  
+        
       </>  
    );
 }
